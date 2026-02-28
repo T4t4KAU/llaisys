89d1ffef382588fd9cf29d1945243b3e486b2882
T4t4KAU <huangwenxuan271828@163.com>
2026-02-28 10:16:04 +0800

    finish homework 1

--- src/tensor/tensor.cpp
+++ src/tensor/tensor.cpp
@@ -164,27 +164,61 @@ void Tensor::debug() const {
 }
 
 bool Tensor::isContiguous() const {
-    TO_BE_IMPLEMENTED();
+    ptrdiff_t expected_stride = 1;
+
+    auto ndim_ = _meta.shape.size();
+    for (size_t i = 1; i <= ndim_; i++) {
+        if (_meta.strides[ndim_ - i] != expected_stride) {
+            return false;
+        }
+
+        expected_stride *= _meta.shape[ndim_ - i];
+    }
+
     return true;
 }
 
 tensor_t Tensor::permute(const std::vector<size_t> &order) const {
-    TO_BE_IMPLEMENTED();
-    return std::shared_ptr<Tensor>(new Tensor(_meta, _storage));
+    std::vector<size_t> new_shape(ndim());
+    std::vector<ptrdiff_t> new_strides(ndim());
+
+    for (size_t i = 0; i < ndim(); i++) {
+        new_shape[i] = _meta.shape[order[i]];
+        new_strides[i] = _meta.strides[order[i]];
+    }
+
+    TensorMeta new_meta{_meta.dtype, new_shape, new_strides};
+    return std::shared_ptr<Tensor>(new Tensor(new_meta, _storage, _offset));
 }
 
 tensor_t Tensor::view(const std::vector<size_t> &shape) const {
-    TO_BE_IMPLEMENTED();
-    return std::shared_ptr<Tensor>(new Tensor(_meta, _storage));
+    const size_t ndim_ = shape.size();
+    std::vector<ptrdiff_t> new_strides(ndim_);
+    ptrdiff_t stride = 1;
+
+    for (size_t i = 1; i <= ndim_; ++i) {
+        new_strides[ndim_ - i] = stride;
+        stride *= static_cast<ptrdiff_t>(shape[ndim_ - i]);
+    }
+
+    TensorMeta new_meta{_meta.dtype, shape, new_strides};
+    return std::shared_ptr<Tensor>(new Tensor(new_meta, _storage, _offset));
 }
 
 tensor_t Tensor::slice(size_t dim, size_t start, size_t end) const {
-    TO_BE_IMPLEMENTED();
-    return std::shared_ptr<Tensor>(new Tensor(_meta, _storage));
+    TensorMeta new_meta = _meta;
+    new_meta.shape[dim] = end - start;
+
+    const ptrdiff_t delta = static_cast<ptrdiff_t>(start) * _meta.strides[dim];
+    const ptrdiff_t new_offset = static_cast<ptrdiff_t>(_offset) + delta;
+    size_t dtype_size = utils::dsize(new_meta.dtype);
+
+    return std::shared_ptr<Tensor>(new Tensor(new_meta, _storage,
+                                              static_cast<size_t>(new_offset) * dtype_size));
 }
 
 void Tensor::load(const void *src_) {
-    TO_BE_IMPLEMENTED();
+    std::memcpy(_storage->memory(), (std::byte *)src_, _storage->size());
 }
 
 tensor_t Tensor::contiguous() const {
